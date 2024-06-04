f3d9f3abc33c76d6e64439879becd105baa58e27
Márcio dos Santos <marcio.economia@ulbra.edu.br>
2024-06-04 10:21:05 -0300

    Update Dashboard.py
    Ajustes na função utilizada pelo pandas devido a versão do mesmo

--- Dashboard.py
+++ Dashboard.py
@@ -45,7 +45,7 @@ receita_estados = dados.groupby('Local da compra')[['Preço']].sum()
 receita_estados = dados.drop_duplicates(subset='Local da compra')[['Local da compra', 'lat', 'lon']].merge(
     receita_estados, left_on='Local da compra', right_index=True).sort_values('Preço', ascending=False)
 
-receita_mensal = dados.set_index('Data da Compra').groupby(pd.Grouper(freq='ME'))['Preço'].sum().reset_index()
+receita_mensal = dados.set_index('Data da Compra').groupby(pd.Grouper(freq='M'))['Preço'].sum().reset_index()
 receita_mensal['Ano'] = receita_mensal['Data da Compra'].dt.year
 receita_mensal['Mes'] = receita_mensal['Data da Compra'].dt.month_name()
 
@@ -57,7 +57,7 @@ vendas_estados = dados.drop_duplicates(subset='Local da compra')[['Local da comp
     vendas_estados, left_on='Local da compra', right_index=True).sort_values('Preço', ascending=False)
 
 vendas_mensal = pd.DataFrame(
-    dados.set_index('Data da Compra').groupby(pd.Grouper(freq='ME'))['Preço'].count()).reset_index()
+    dados.set_index('Data da Compra').groupby(pd.Grouper(freq='M'))['Preço'].count()).reset_index()
 vendas_mensal['Ano'] = vendas_mensal['Data da Compra'].dt.year
 vendas_mensal['Mes'] = vendas_mensal['Data da Compra'].dt.month_name()
 
